cf6e25d4aab3920f7dbc1bd0b0c282c16c903b16
luis <gutierrez.luis@icloud.com>
2025-09-17 19:24:44 -0700

    Add files via upload

--- make_html.sh
+++ make_html.sh
@@ -6,11 +6,11 @@
 cat <<EOF > index.html
 <!DOCTYPE html>
 <html>
-<head>
-  <title>My Template</title>
-</head>
-<body>
-  <h1>Hello, world!</h1>
-</body>
+  <head>
+    <title>My Template</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+  </body>
 </html>
 EOF
