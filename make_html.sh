9b2906d05fdda5640f57d0084f2d63b77885db2e
luis <gutierrez.luis@icloud.com>
2025-09-17 19:02:08 -0700

    Add files via upload

---
+++ make_html.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# write an html template in the same working directory
+
+
+cat <<EOF > index.html
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My Template</title>
+</head>
+<body>
+  <h1>Hello, world!</h1>
+</body>
+</html>
+EOF
