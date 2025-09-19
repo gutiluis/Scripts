6cb6f055fc9dbee4cd4c396fafc3e0202b3bcb6f
luis <gutierrez.luis@icloud.com>
2025-09-18 17:06:21 -0700

    Add files via upload

--- make_html.sh
+++ make_html.sh
@@ -2,13 +2,18 @@
 
 # write an html template in the same working directory
 
-
 cat <<EOF > index.html
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
-    <title>[INFO]Write a browser header...</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>[INFO]Write an html5 browser header...</title>
+    <link rel="stylesheet" href="style.css">
   </head>
-  <body></body>
+  <body>
+    <script src="index.js"></script>
+  </body>
 </html>
 EOF
